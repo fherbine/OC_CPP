ebeb7adad4253bfb9d454d61ae7090313b5afed8
fherbine <felix.herbinet@yahoo.com>
2018-02-11 23:29:04 +0100

    first modifications of 02 - main.cpp

---
+++ 02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int premiereVariableCpp(0);
+	int autreVariableEntiere(2147483647);
+
+	double variableDecimale(42.19);
+
+	bool estCeVrai(false);
+
+	char lettre('a');
+	return (0);
+}
