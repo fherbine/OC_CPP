001133c1aadb08a107ec23bf1adc376062d2fdd1
fherbine <felix.herbinet@yahoo.com>
2018-02-11 23:06:20 +0100

    implementation of comments on the first main of the first directory

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
+/*
+	First program in C++ made by me, using OpenClassrooms course
+	Compiling using g++ !
+*/
 
-using namespace std;
+#include <iostream> // including iostream lib
+
+using namespace std; // using standard namespace
 
 int main(void)
 {
+	/*
+		Let's display a short message
+	*/
 	cout << "Hello world !" << endl << "Salut le monde !" << endl;
 	return (0);
 }
