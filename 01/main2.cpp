469f7518a9196d6783eebff027bd10586f66db4b
fherbine <felix.herbinet@yahoo.com>
2018-02-11 22:59:16 +0100

    adding the 2nd test which display a sentence.

---
+++ 01/main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Ceci est le deuxieme test";
+	cout << " d'une longue serie" << endl;
+	cout << "Enjoy ! :)" << endl;
+	return (0);
+}
