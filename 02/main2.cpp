958ca5196fb1bae57a1fdc0da7f98fcd32353268
fherbine <felix.herbinet@yahoo.com>
2018-02-11 23:41:36 +0100

    first modifications on 02 - main2.cpp

---
+++ 02/main2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	int variableToDisplay(42);
+
+	cout << "And the answer to the universe is : " << variableToDisplay << endl;
+	return (0);
+}
