86ac63d9ee89db33864723c2b2ec304419be477b
fherbine <felix.herbinet@yahoo.com>
2018-02-17 18:52:10 +0100

    finnishing 05 - big_swap

--- 05/big_swap.cpp
+++ 05/big_swap.cpp
@@ -1,5 +1,26 @@
-#include "header.h"
+#include <iostream>
 
 using namespace std;
 
+void	big_s(int& a, int& b)
+{
+	int	tmp(0);
 
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int		main(void)
+{
+	int	n1(0), n2(0);
+
+	cout << "Choose two numbers to swap !" << endl << "The first one : ";
+	cin >> n1;
+	cout << "Then the second : ";
+	cin >> n2;
+	big_s(n1, n2);
+	cout << "first's value : " << n1 << endl;
+	cout << "second's value : " << n2 << endl;
+	return (0);
+}
