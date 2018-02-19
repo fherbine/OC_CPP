65b3b8761586e0b6756d00ffadbfdad61d57238e
fherbine <felix.herbinet@yahoo.com>
2018-02-19 02:24:41 +0100

    Adding routines lines on 06/dymnamic/main.cpp

---
+++ 06/dynamic/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector> // Lib needed to create & manipulate dynamic tables
+
+/*
+ * Main cpp file
+ */
+
+using namespace std;
+
+int main(void)
+{
+	return (0);
+}
