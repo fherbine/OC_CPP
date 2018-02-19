08a31926517081470fe96111692ac267ab600a5e
fherbine <felix.herbinet@yahoo.com>
2018-02-19 02:17:51 +0100

    Updating 06/static - main.cpp

--- 06/static/main.cpp
+++ 06/static/main.cpp
@@ -1,12 +1,28 @@
+#include "header.h"
+
 /*
  * Main file for static program
  */
-
-#include "header.h"
 
 using namespace std;
 
 int		main(void)
 {
+	int		ct_size(4), st_size(3);
+	double 	cos_tab[ct_size];
+	string	str_tab[st_size];
+
+	cos_tab[0] = 3;
+	cos_tab[1] = 2;
+	cos_tab[2] = 1;
+	cos_tab[3] = 0;
+
+	str_tab[0] = "42";
+	str_tab[1] = " is the answer to ";
+	str_tab[2] = "unviverse !";
+
+	all_cos(cos_tab, ct_size);
+	all_names(str_tab, st_size);
+
 	return (0);
 }
