b1d70ee8c8cc8b5d2645c2086268e263d6c5d080
fherbine <felix.herbinet@yahoo.com>
2018-02-19 02:32:57 +0100

    Adding basics features on 06/dynamic

--- 06/dynamic/main.cpp
+++ 06/dynamic/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector> // Lib needed to create & manipulate dynamic tables
+#include <string>
 
 /*
  * Main cpp file
@@ -7,7 +8,25 @@
 
 using namespace std;
 
+void	display_str_tab(vector<string> tab, int size)
+{
+	for (int i(0); i < size; ++i)
+		cout << "String is : " << tab[i] << endl;
+}
+
+void	display_int_tab(vector<int> tab, int size)
+{
+	for (int i(0); i < size; ++i)
+		cout << tab[i] << '\t';
+	cout << endl;
+}
+
 int main(void)
 {
+	vector<int>		int_tab(42, 42);
+	vector<string>	str_tab(12, "Default");
+
+	display_int_tab(int_tab, 42);
+	display_str_tab(str_tab, 12);
 	return (0);
 }
