8332c202fd9254ae42420fb8a9161072e2c29405
fherbine <felix.herbinet@yahoo.com>
2018-02-19 02:00:20 +0100

    Updating 06/static - main.cpp & header.h with usual lines

---
+++ 06/static/header.h
@@ -0,0 +1,13 @@
+/*
+ * Header file for static program
+ */
+
+#ifndef HEADER_H_INCLUDED
+
+# define HEADER_H_INCLUDED
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+#endif
---
+++ 06/static/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * Main file for static program
+ */
+
+#include "header.h"
+
+using namespace std;
+
+int		main(void)
+{
+	return (0);
+}
