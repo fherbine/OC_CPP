5d656ef615b1dae9ac740e5d36772396525e029c
fherbine <felix.herbinet@yahoo.com>
2018-02-20 01:18:28 +0100

    Updating 07 - main.cpp : basic user writing

--- 07/main.cpp
+++ 07/main.cpp
@@ -7,13 +7,19 @@ using namespace std;
 int main(void)
 {
 	string		pathName("./");
+	string		tmp("");
 
 	cout << "File path: ";
 	getline(cin, pathName);
 	cout << "PATH_TO_OPEN: " << pathName << endl;
 	ofstream	path(pathName.c_str());
 	if (path)
-	{}
+	{
+		cout << "What do you want to write in this file ?" << endl << "$> ";
+		getline(cin, tmp);
+		path << tmp << endl;
+		path << "OK - Exiting ..." << endl;
+	}
 	else
 	{
 		cout << "\033[1;31;40mError :\033[0m program failed to open: ";
